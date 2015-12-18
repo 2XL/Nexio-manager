93d4967f1882be6bd0f3e7f8275f77b8810beaac
chenglong zou <chenglong.zq@gmail.com>
2015-12-18 18:08:28 +0100

    Added mysql test api working

--- test/test-connectdb.js
+++ test/test-connectdb.js
@@ -1,7 +1,31 @@
 #!/usr/bin/nodejs
 
 var mysql = require("mysql");
-var constants = require("constants");
+var constants = require("../constants");
+
+console.log(constants)
+
+
+var connection = mysql.createConnection(constants.mysql.login);
+
+
+connection.connect();
+var query = "show tables";
+connection.query(query, function( err, rows, fields){
+    if(err) throw err;
+
+    console.log(fields);
+    rows.forEach(function(item){
+        console.log(item);
+    });
+});
+
+
+
+connection.end();
+
+
+
 
 
 console.log("test.connectdb.js");
